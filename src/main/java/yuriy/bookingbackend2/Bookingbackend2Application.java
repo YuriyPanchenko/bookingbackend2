23c7c6500c1b75b9b469343b8c3c4dc87a49b8d1
YuriyPanchenko <yuriy.panchenko.2000@gmail.com>
2020-02-27 15:06:55 +0200

    resources adn application.java))

---
+++ src/main/java/yuriy/bookingbackend2/Bookingbackend2Application.java
@@ -0,0 +1,13 @@
+package yuriy.bookingbackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bookingbackend2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bookingbackend2Application.class, args);
+    }
+
+}
